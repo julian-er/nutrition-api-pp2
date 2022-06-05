30c3c82e1d251cf38a8c191f23670ec05f244796
Julián Rosalen <rosalenjulian@gmail.com>
2022-06-05 12:55:14 -0300

    Sql queries added

---
+++ database/data_test_inserts.sql
@@ -0,0 +1,6 @@
+INSERT INTO nutritionist ( name, last_name, user, email, password) VALUES 
+('Michelle', 'Lemoine', 'mlemoine', 'mlemoine@gmail.com', '1111')
+('Michelle', 'Lemoine', 'mlemoine', 'mlemoine@gmail.com', '1111')
+('Michelle', 'Lemoine', 'mlemoine', 'mlemoine@gmail.com', '1111')
+('Michelle', 'Lemoine', 'mlemoine', 'mlemoine@gmail.com', '1111')
+('Michelle', 'Lemoine', 'mlemoine', 'mlemoine@gmail.com', '1111')
