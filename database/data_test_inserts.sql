0afe242c8174bcb0778498c01503767788face51
AdanVolken <90329028+AdanVolken@users.noreply.github.com>
2022-09-06 12:10:03 -0300

    Update data_test_inserts.sql

--- database/data_test_inserts.sql
+++ database/data_test_inserts.sql
@@ -10,5 +10,5 @@ INSERT INTO patients ( name, last_name) VALUES
 ('Julian', 'Rosalen')
 
 INSERT INTO food ( name_food, description, link_photo ) VALUES
-('Pera', 'Fruta originaria de america del sur, de color amarrillo y alta en proteina','adjuntar foto pera')
+('Pera', 'Fruta originaria de america del sur, de color amarillo y alta en proteina','adjuntar foto pera')
 ('Manzana', 'Fruta de color rojo y alta en proteina','adjuntar foto manzana')
