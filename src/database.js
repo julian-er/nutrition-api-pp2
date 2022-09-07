0ad87ff8a0943e26edb6ac1d1e41986473627bd2
gobu-github <hgobulin.ds@gmail.com>
2022-09-07 12:58:53 -0300

    Add database.js file without changes

--- src/database.js
+++ src/database.js
@@ -1,23 +1,23 @@
-const mysql = require("mysql");
-const chalk = require("chalk");
+const mysql = require('mysql');
+const chalk = require('chalk');
 
 //Create connection to MySQL Server
 const mysqlConnection = mysql.createConnection({
-  host: "localhost",
-  user: "root", // User for yout SQL Server
-  password: "1402", //Pasword for your SQL Server
-  database: "db_nutrition",
+	host: 'localhost',
+	user: 'root', // User for yout SQL Server
+	password: '', //Pasword for your SQL Server
+	database: 'db_nutrition',
 });
 
 mysqlConnection.connect(function (err) {
-  if (!err) {
-    console.log("Conection to database", chalk.bgGreen("Succes!"));
-  } else {
-    console.log("Conection to database", chalk.bgRed("Fail!"));
-    console.group();
-    console.log(chalk.red("The error shown below: "));
-    console.log(err.message);
-  }
+	if (!err) {
+		console.log('Conection to database', chalk.bgGreen('Succes!'));
+	} else {
+		console.log('Conection to database', chalk.bgRed('Fail!'));
+		console.group();
+		console.log(chalk.red('The error shown below: '));
+		console.log(err.message);
+	}
 });
 
 module.exports = mysqlConnection; //Export module for use into other files
