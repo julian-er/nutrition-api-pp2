f1722d258600460528bf70ff22b6c8e2b6f16d43
gobu-github <hgobulin.ds@gmail.com>
2022-09-07 13:14:03 -0300

    Changes to the Get and Post Methods in the nutritionists.js file

--- src/routes/nutritionists.js
+++ src/routes/nutritionists.js
@@ -1,104 +1,145 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const mysqlConnection = require("../database");
+const mysqlConnection = require('../database');
 
 //#region GetMethods
-router.get("/nutritionists", (req, res) => {
-  const query = ` SELECT * FROM nutritionist `;
+router.get('/nutritionists', (req, res) => {
+	const query = ` SELECT * FROM nutritionist `;
 
-  mysqlConnection.query(query, (err, rows, fields) => {
-    if (!err) {
-      res.status(200).json(rows);
-    } else {
-      res.status(500).json({
-        message: "Sorry we have an unespected error",
-        error : err.sqlMessage
-      });
-    }
-  });
+	mysqlConnection.query(query, (err, rows, fields) => {
+		if (!err) {
+			res.status(200).json(rows);
+		} else {
+			res.status(500).json({
+				message: 'Sorry we have an unespected error',
+				error: err.sqlMessage,
+			});
+		}
+	});
 });
 
-router.get("/nutritionists/:id", (req, res) => {
-  const { id } = req.params; // Also you can use this other notation req.params.id to see the param
-  const query = ` SELECT * FROM nutritionist WHERE id = ? `;
+router.get('/nutritionists/:id', (req, res) => {
+	const { id } = req.params; // Also you can use this other notation req.params.id to see the param
+	const query = ` SELECT * FROM nutritionist WHERE id = ? `;
 
-  mysqlConnection.query(query, [id], (err, rows, fields) => {
-    if (!err) {
-      res.status(200).json(rows);
-    } else {
-      res.status(500).json({
-        message: "Sorry we have an unespected error",
-        error: err.sqlMessage
-      });
-    }
-  });
+	mysqlConnection.query(query, [id], (err, rows, fields) => {
+		if (!err) {
+			res.status(200).json(rows);
+		} else {
+			res.status(500).json({
+				message: 'Sorry we have an unespected error',
+				error: err.sqlMessage,
+			});
+		}
+	});
 });
 
 //#endregion
 
 //#region PostMethods
-router.post("/nutritionists", (req, res) => {
-  const { name, last_name, user, email, password } = req.body;
-  const query = `INSERT INTO nutritionist ( name, last_name, user, email, password) VALUES  (? , ? , ? , ? , ?)`;
+router.post('/nutritionists', (req, res) => {
+	const {
+		name,
+		last_name,
+		user,
+		password,
+		email,
+		registration_number,
+		phone_number,
+		birth_date,
+	} = req.body;
+	const query = `INSERT INTO nutritionist (name, last_name, user, password, email, registration_number, phone_number, birth_date) VALUES  (?, ?, ?, ?, ?, ?, ?, ?)`;
 
-  mysqlConnection.query(
-    query,
-    [name, last_name, user, email, password],
-    (err, rows, fields) => {
-      if (!err) {
-        res.status(200).json({
-          message: "Your nutritionis was created successfully",
-        });
-      } else {
-        res.status(500).json({
-          message: "Sorry we have an unespected error",
-          error: err.sqlMessage
-        })
-      }
-    }
-  );
+	mysqlConnection.query(
+		query,
+		[
+			name,
+			last_name,
+			user,
+			password,
+			email,
+			registration_number,
+			phone_number,
+			birth_date,
+		],
+		(err, rows, fields) => {
+			if (!err) {
+				res.status(200).json({
+					message: 'Your nutritionist was created successfully',
+				});
+			} else {
+				res.status(500).json({
+					message: 'Sorry we have an unespected error',
+					error: err.sqlMessage,
+				});
+			}
+		}
+	);
 });
 //#endregion
 
 //#region PutMethods
-router.put("/nutritionists/:id", (req, res) => {
-  const { id } = req.params;
-  const { name, last_name } = req.body;
-  const query = `UPDATE nutritionist SET name = ? , last_name = ? WHERE id = ?`;
+router.put('/nutritionists/:id', (req, res) => {
+	const { id } = req.params;
+	const {
+		name,
+		last_name,
+		user,
+		password,
+		email,
+		registration_number,
+		phone_number,
+		birth_date,
+	} = req.body;
+	const query = `UPDATE nutritionist SET name = ?, last_name = ?, user = ?, password = ?,	email = ?, registration_number = ?, phone_number = ?, birth_date = ? WHERE id = ?`;
 
-  mysqlConnection.query(query, [name, last_name, id], (err, rows, fields) => {
-    if (!err) {
-      res.status(200).json({
-        message: "Your nutritionis was updated successfully",
-      });
-    } else {
-      res.status(500).json({
-        message: "Sorry we have an unespected error",
-        error: err.sqlMessage
-      })
-    }
-  });
+	mysqlConnection.query(
+		query,
+		[
+			name,
+			last_name,
+			user,
+			password,
+			email,
+			registration_number,
+			phone_number,
+			birth_date,
+			id,
+		],
+		(err, rows, fields) => {
+			if (!err) {
+				res.status(200).json({
+					message: 'Your nutritionis was updated successfully',
+				});
+			} else {
+				res.status(500).json({
+					message: 'Sorry we have an unespected error',
+					error: err.sqlMessage,
+				});
+			}
+		}
+	);
 });
 //#endregion
 
 //#region DeleteMethods
-router.delete("/nutritionists/:id", (req, res) => {
-  const { id } = req.params;
-  const query = `DELETE FROM nutritionist WHERE id = ?`;
+router.delete('/nutritionists/:id', (req, res) => {
+	const { id } = req.params;
+	const query = `DELETE FROM nutritionist WHERE id = ?`;
 
-  mysqlConnection.query(query, [id], (err, rows, fields) => {
-    if (!err) {
-      res.status(200).json({
-        message: "Your nutritionis was deleted successfully",
-      });
-    } else {
-      res.status(500).json({
-        message: "Sorry we have an unespected error",
-        error : err.sqlMessage
-      })
-    }
-  });
+	mysqlConnection.query(query, [id], (err, rows, fields) => {
+		if (!err) {
+			res.status(200).json({
+				message: 'Your nutritionis was deleted successfully',
+			});
+		} else {
+			res.status(500).json({
+				message: 'Sorry we have an unespected error',
+				error: err.sqlMessage,
+			});
+		}
+	});
 });
 //#endregion
 module.exports = router;
