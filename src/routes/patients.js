21c24eedbf0d91420f681f912d9bd53c11ae6a66
Julián Rosalen <rosalenjulian@gmail.com>
2022-09-12 21:12:16 -0300

    Endpoints added

--- src/routes/patients.js
+++ src/routes/patients.js
@@ -10,7 +10,11 @@ export function setPatientsRoutes(router) {
 
 	router.get('/patients', (req, res) => controller.getPatients(req, res));
 	router.get('/patients/:id', (req, res) => controller.getUserById(req, res));
+	router.get('/patients/user/:user_name', (req, res) => controller.getUserByUserName(req, res));
 	router.post('/patients', (req, res) => controller.createUser(req, res));
 	router.put('/patients/:id', (req, res) => controller.editUser(req, res));
+	router.put('/patients/update-user/:id', (req, res) => controller.changeUserName(req, res));
+	router.put('/patients/update-mail/:id', (req, res) => controller.changeMail(req, res));
+	router.put('/patients/update-password/:id', (req, res) => controller.changePassword(req, res));
 	router.delete('/patients/:id', (req, res) => controller.deleteUser(req, res));
 }
