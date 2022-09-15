a2bb5e1d5f90256824bbd1734f75d9ceb700996f
Julián Rosalen <rosalenjulian@gmail.com>
2022-09-14 22:12:54 -0300

    users routes -> create route changed

--- src/routes/users.js
+++ src/routes/users.js
@@ -11,7 +11,7 @@ export function setUserRoutes(router) {
 	router.get('/users', (req, res) => controller.getUsers(req, res));
 	router.get('/users/:id', (req, res) => controller.getUserById(req, res));
 	router.get('/users/user/:user_name', (req, res) => controller.getUserByUserName({user_name : req.params.user_name}, res));
-	router.post('/users', (req, res) => controller.createUser(req, res));
+	router.post('/users/create', (req, res) => controller.createUser(req, res));
 	router.put('/users/:id', (req, res) => controller.editUser(req, res));
 	router.put('/users/update-user/:id', (req, res) => controller.changeUserName(req, res));
 	router.put('/users/update-mail/:id', (req, res) => controller.changeMail(req, res));
