52c8f0d8b5d016da40f9c1bf036db200c80c0243
AdanVolken <90329028+AdanVolken@users.noreply.github.com>
2022-09-18 19:12:23 -0300

    adding diets to controllers and routes

--- src/routes/allergies.js
+++ src/routes/allergies.js
@@ -10,12 +10,10 @@ export function setAllergiesRoutes(router) {
 
 	router.get('/allergies', (req, res) => controller.getAllergies(req, res));
 	router.post('/allergies/create', (req, res) => controller.createAllergy(req, res));
-<<<<<<< HEAD
 	router.put('/allergies/:id', (req, res) => controller.editAllergy(req, res));
 	router.delete('/allergies/:id', (req, res) => controller.deleteAllergy(req, res));
-=======
 	router.put('/allergies/update/:id', (req, res) => controller.editAllergy(req, res));
 	router.delete('/allergies/delete/:id', (req, res) => controller.deleteAllergy(req, res));
 
->>>>>>> 6e0b3b3cf8acee6ab89ed10274a6308f47ed6b09
+
 }
