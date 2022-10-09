1e7bd9f7c0214d6337a144e70f2efecd15949fa9
Adan Volken <rolfivolken9@gmail.com>
2022-10-09 19:43:06 -0300

    WIP

--- src/controllers/users/patients.js
+++ src/controllers/users/patients.js
@@ -19,4 +19,23 @@ export class PatientsController extends UsersController {
 			error => res.status(500).json(error)
 		);
 	}
+
+	
+	async getNutritionistPatients(req, res){
+		const { id } = req.params;
+		const query= `
+		SELECT u.user_name, u.first_name, u.email, u.last_name, u.phone_number, u.birth_date, u.profile_image
+		FROM nutritionist_patients_relation nr
+		JOIN user u
+		ON u.id = nr.patient_id
+		WHERE nr.nutritionist_id = ?
+`
+		this.getById(
+			query,
+			id,
+			response => res.status(200).json(response),
+			error => res.status(500).json(error)
+		);
+	}
+	
 }
